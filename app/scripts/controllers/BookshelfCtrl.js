f39139ff37d42e455242b02ebea9400b723ba632
Naum Krivoruk <naum.krivoruk@gmail.com>
2014-01-22 09:20:58 +0200

    bookshelf.html changes

--- app/scripts/controllers/BookshelfCtrl.js
+++ app/scripts/controllers/BookshelfCtrl.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('readingRoomApp')
-  .controller('BookshelfCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('BookshelfCtrl', function ($scope, userSrvc) {
+    $scope.reader = userSrvc.getUser();
   });
